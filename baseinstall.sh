99a4b1648d1a799486a821637a5d9fc98b78062e
JWalk9000 <j.turbo.j@gmail.com>
2024-12-24 14:01:41 -0600

    revised fix for force over write of partitions: fixed syntax

--- baseinstall.sh
+++ baseinstall.sh
@@ -59,10 +59,9 @@ else
 fi
 
 echo "=> Formatting EFI partition as FAT32"
-mkfs.fat -F32 "$EFI_PART" $FORCE_FLAG
-
+mkfs.fat -F 32 $FORCE_FLAG "$EFI_PART" 
 echo "=> Formatting primary partition as Btrfs"
-mkfs.btrfs "$BTRFS_PART" $FORCE_FLAG
+mkfs.btrfs $FORCE_FLAG "$BTRFS_PART" 
 
 # 7. Create and mount Btrfs subvolumes
 echo "=> Mounting $BTRFS_PART to /mnt"
