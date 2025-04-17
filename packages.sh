14610e27438f73b4c528d8a567b1169efb125f1a
JWalk9000 <j.turbo.j@gmail.com>
2025-04-17 14:59:09 -0500

    fix creation of userpkgs.yml

--- packages.sh
+++ packages.sh
@@ -254,16 +254,16 @@ display_services() {
 save_userpkgs() {
   info_print "Saving user-defined packages and services to $USER_YAML..."
   sleep 1.5
-  yq -i '.packages.user = []' "$USER_YAML"
+  yq -i -y '.packages.user = []' "$USER_YAML"
   sleep 1.5
-  yq -i '.services.user = []' "$USER_YAML"
+  yq -i -y '.services.user = []' "$USER_YAML"
   sleep 1.5
   for PKG in "${USERPKGS[@]}"; do
-    yq -i '.packages.user += ["'$PKG'"]' "$USER_YAML"
+    yq -i -y '.packages.user += ["'$PKG'"]' "$USER_YAML"
     sleep 1.5
   done
   for SVC in "${USER_SVCS[@]}"; do
-    yq -i '.services.user += ["'$SVC'"]' "$USER_YAML"
+    yq -i -y '.services.user += ["'$SVC'"]' "$USER_YAML"
     sleep 1.5
   done
   info_print "User configuration saved."
